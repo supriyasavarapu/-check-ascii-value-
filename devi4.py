c1396226c48902ed17efc98d310cb132755d7fd1
supriyasavarapu <146428159+supriyasavarapu@users.noreply.github.com>
2025-07-08 20:50:05 +0700

    Add files via upload

---
+++ devi4.py
@@ -0,0 +1,20 @@
+# n=input("enter word")
+# value=int(input("enter value"))
+# for i in n:
+#   if (ord(i)==value):
+#         print("true the character exist")
+#         print(i)
+#   else:
+#         print("the character is not exist")                                             
+                                                                                        #o/p=enter wordgoogle
+#     #o/p=   enter wordgoogle                                                           #    enter value113
+#             enter value103                                                                # the character is not exist
+#             true the character exist                                                      #  the character is not exist
+#             g                                                                             #  the character is not exist
+#             the character is not exist                                                     #  the character is not exist
+#             the character is not exist                                                      #  the character is not exist
+#             true the character exist                                                        # the character is not exist
+#             g
+#             the character is not exist 
+#             the character is not exist 
+#
